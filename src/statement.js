16be7d34ef2889a2f41db0f93d1b258f404a5ae4
Renato Filho <renatosousafilho@gmail.com>
2023-06-13 09:27:48 -0300

    Removing the play variable

--- src/statement.js
+++ src/statement.js
@@ -1,3 +1,7 @@
+
+
+
+
 function statement (invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -6,34 +10,41 @@ function statement (invoice, plays) {
                         { style: "currency", currency: "USD",
                           minimumFractionDigits: 2 }).format;
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
-    case "tragedy":
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
-      }
-      break;
-    case "comedy":
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
-      }
-      thisAmount += 300 * perf.audience;
-      break;
-    default:
-        throw new Error(`unknown type: ${play.type}`);
+    function playFor(aPerformance) {
+      return plays[aPerformance.playID];
     }
+
+    function amountFor(aPerformance) {
+      let result = 0;
+      switch (playFor(perf).type) {
+        case "tragedy":
+          result = 40000;
+          if (aPerformance.audience > 30) {
+            result += 1000 * (aPerformance.audience - 30);
+          }
+          break;
+        case "comedy":
+          result = 30000;
+          if (aPerformance.audience > 20) {
+            result += 10000 + 500 * (aPerformance.audience - 20);
+          }
+          result += 300 * aPerformance.audience;
+          break;
+        default:
+            throw new Error(`unknown type: ${playFor(perf).type}`);
+        }
+        return result;
+    }
+
+    let thisAmount = amountFor(perf);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // print line for this order
-    result += `  ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+    result += `  ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
   }
   result += `Amount owed is ${format(totalAmount/100)}\n`;
