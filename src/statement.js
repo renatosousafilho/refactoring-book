24f382831bf8b4586e9c1cc61ab9e93524cfbe97
Renato Filho <renatosousafilho@gmail.com>
2023-06-14 22:01:04 -0300

    Tirando as funções de dentro do for

--- src/statement.js
+++ src/statement.js
@@ -7,44 +7,45 @@ function usd(aNumber) {
 }
 
 function statement(invoice, plays) {
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case 'tragedy':
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case 'comedy':
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+    }
+    return result;
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ('comedy' === playFor(aPerformance).type)
+      result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
+
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
+
   for (let perf of invoice.performances) {
-    function playFor(aPerformance) {
-      return plays[aPerformance.playID];
-    }
-
-    function amountFor(aPerformance) {
-      let result = 0;
-      switch (playFor(perf).type) {
-        case 'tragedy':
-          result = 40000;
-          if (aPerformance.audience > 30) {
-            result += 1000 * (aPerformance.audience - 30);
-          }
-          break;
-        case 'comedy':
-          result = 30000;
-          if (aPerformance.audience > 20) {
-            result += 10000 + 500 * (aPerformance.audience - 20);
-          }
-          result += 300 * aPerformance.audience;
-          break;
-        default:
-          throw new Error(`unknown type: ${playFor(perf).type}`);
-      }
-      return result;
-    }
-
-    function volumeCreditsFor(aPerformance) {
-      let result = 0;
-      result += Math.max(aPerformance.audience - 30, 0);
-      if ('comedy' === playFor(aPerformance).type)
-        result += Math.floor(aPerformance.audience / 5);
-      return result;
-    }
-
     volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
