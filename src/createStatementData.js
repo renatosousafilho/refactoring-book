5ea6cce7011a4d42ff84adb544833be28452c439
Renato Filho <renatosousafilho@gmail.com>
2023-06-15 08:45:36 -0300

    moving function volumeCreditsFor to method volumeCredits

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -25,6 +25,14 @@ class PerformanceCalculator {
     }
     return result;
   }
+
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+    if ('comedy' === this.play.type)
+      result += Math.floor(this.performance.audience / 5);
+    return result;
+  }
 }
 
 function createStatementData(invoice, plays) {
@@ -52,11 +60,8 @@ function createStatementData(invoice, plays) {
   }
 
   function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ('comedy' === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+      .volumeCredits;
   }
 
   function totalVolumeCredits(data) {
